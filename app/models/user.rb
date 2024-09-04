97c1165c8b969e5e351a5344ded95403f7593329
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-09-04 15:57:34 +0000

    フォロー・フォロワー機能 btn実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_one_attached :profile_image
+  
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
-  has_many :active_relationships, active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
-  has_many :passive_relationships, active_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   
-  has_many :followings, through: :active_relationships, source: :followed
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   
-  has_many :followers, through: :passive_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
   
+  has_one_attached :profile_image
   
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
@@ -27,11 +28,11 @@ class User < ApplicationRecord
   end
   
   def follow(user_id)
-    active_relationships.create(followed_id: user_id)
+    relationships.create(followed_id: user_id)
   end 
   
   def unfollow(user_id)
-    active_relationships.find_by(followed_id: user_id).destroy
+    relationships.find_by(followed_id: user_id).destroy
   end 
   
   def following?(user)
--- db/migrate/20240903060149_create_favorites.rb
+++ db/migrate/20240903060149_create_favorites.rb
@@ -4,6 +4,7 @@ class CreateFavorites < ActiveRecord::Migration[6.1]
       t.integer :user_id
       t.integer :book_id
 
+
       t.timestamps
     end
   end
