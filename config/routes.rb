ba97429bbe91a70551aeecec850cc11e0bfec6eb
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-09-06 06:59:23 +0000

    検索機能実装中　テスト

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     get "followers" => "relationships#followers", as: "followers"
   end 
   
-  get "searche" => "searches#search"
+  get "search" => "searches#search"
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
