a81a81877ae72dba877914505ce4c5bf5d8902d0
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-09-08 03:20:24 +0000

    コメント機能非同期化

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,12 +1,12 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
   
-  def search_result
+  def search
     @model = params[:model]
     @content = params[:content]
     @method = params[:method]
     
-    if @model == "User"
+    if @model == "user"
       @records = User.search_for(@content, @method)
     else
       @records = Book.search_for(@content, @method)
